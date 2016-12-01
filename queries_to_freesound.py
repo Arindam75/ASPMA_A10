088262ba76950c2d1919f53950ffc110c8377489
Helena Cuesta <helenacumu@gmail.com>
2016-12-01 18:40:01 +0100

    finish downloading sound

--- queries_to_freesound.py
+++ queries_to_freesound.py
@@ -1,11 +1,13 @@
 import soundDownload as SD
+import numpy as np
 
-queryText = ["violin","cello note","bassoon","flute","trumpet note", "snare hit", "naobo", "daluo","mridangam stroke","clarinet"]
+queryText = ["violin","cello_note","bassoon","flute","trumpet_note", "snare_hit", "naobo", "daluo","mridangam_stroke","clarinet"]
 tag=["single-note","good-sounds","tenuto","good-sounds","single-note","one-shot","qmul","qmul","sm-58","single-note"]
 duration=(0,5)
 API_Key='oRDjiagAmLlrlxnxwKs6MOqDCIlCa0Xi5H5mu1VH'
-outputDir='testDownload/'
+outputDir='downloaded/'
 
+vec = np.arange(10)
 
-
-SD.downloadSoundsFreesound(queryText = queryText, tag=tag, duration=duration, API_Key = API_Key, outputDir = outputDir, topNResults = 20, featureExt = '.json')
+for i in vec:
+    SD.downloadSoundsFreesound(queryText = queryText[i], tag=tag[i], duration=duration, API_Key = API_Key, outputDir = outputDir, topNResults = 20, featureExt = '.json')
